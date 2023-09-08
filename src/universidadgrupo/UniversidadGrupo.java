436cb6bace8cb7978a93dbe717c968a435530df4
jule <juliangrenon@gmail.com>
2023-09-08 17:51:26 -0300

    buscarAlumnoxID

--- src/universidadgrupo/UniversidadGrupo.java
+++ src/universidadgrupo/UniversidadGrupo.java
@@ -18,11 +18,20 @@ public class UniversidadGrupo {
     public static void main(String[] args) {
         // PRUEBA DE METODOS ALUMNODATA PARTE 1
         /*//Connection con= Conexion.getConexion();
-        Alumno alumno = new Alumno(14, 12345, "Luna", "Juan Pedro", LocalDate.of(1990, 4, 25), true);
+        //Alumno alumno = new Alumno(123456, "Perez", "Juan", LocalDate.of(1996, 6, 10), true);
         AlumnoData alu = new AlumnoData();
         //alu.guardarAlumno(alumno);
         //alu.modificarAlumno(alumno);
-        alu.eliminarAlumno(14);*/
+        //alu.eliminarAlumno(14);
+        
+        
+        //PRUEBA BUSCAR ALUMNO POR ID
+       Alumno alumnoEncontrado=alu.buscarAlumno(15);
+       if (alumnoEncontrado!=null){
+        System.out.println("dni: "+alumnoEncontrado.getDni());
+        System.out.println("apellido: "+alumnoEncontrado.getApellido());
+       }*/
+        
     }
     
 }
