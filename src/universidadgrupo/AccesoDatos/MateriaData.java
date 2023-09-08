d2dd6a8e6452d884c5ed7a35a134bf2d052807e5
MariiiM <marinamereles@hotmail.com>
2023-09-08 17:28:44 -0300

    Update MateriaData.java
    Actualizacion1 metodo guardarMateria

--- src/universidadgrupo/AccesoDatos/MateriaData.java
+++ src/universidadgrupo/AccesoDatos/MateriaData.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadgrupo.AccesoDatos;
 
-/**
- *
- * @author m
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import universidadgrupo.entidades.Materia;
+
+
 public class MateriaData {
     
+    private Connection con=null;
+    
+    public MateriaData(){
+    
+        con=Conexion.getConexion();
+    }
+    
+    public void guardarMateria (Materia materia){
+    
+        String sql= "INSERT INTO materia(`nombre`, `anio`, `estado`) "
+                + "VALUES (?,?,?)";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnio());
+            ps.setBoolean(3, materia.isEstado());
+            ps.executeUpdate();
+            
+            ResultSet rs=ps.getGeneratedKeys();
+            if(rs.next()){
+            
+                materia.setIdMateria(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Materia guardada");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia");
+        }
+    }
 }
