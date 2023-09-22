a6016ea0d22ed02f581885f5837d21ce4acf5d5f
MariiiM <marinamereles@hotmail.com>
2023-09-22 15:25:57 -0300

    UltimaActualizacion

--- src/universidadgrupo/UniversidadGrupo.java
+++ src/universidadgrupo/UniversidadGrupo.java
@@ -20,11 +20,7 @@ public class UniversidadGrupo {
 
    
     public static void main(String[] args) {
-     
-    Materia materia= new Materia(8,"Frances",3, true);
-     MateriaData mat= new MateriaData();
-     //mat.guardarMateria(materia);
-    mat.modificarMateria(materia);
+  
     }
         }
 
